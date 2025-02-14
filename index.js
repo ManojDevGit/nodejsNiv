b10d62976211809b7839da4014e524e624b0a4a6
Manoj Devadiga <manoj.devadiga@niveussolutions.com>
2025-02-14 10:36:31 +0530

    Create a file using fs module and async/await.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // STEP 1: Create a node project
 // STEP 2: create a promoise to display a mesaage and cal the promise.
+const fs = require("fs").promises;
 
 const promiseMsg = () => {
   return new Promise((resolve, reject) => {
@@ -15,8 +16,6 @@ promiseMsg().then((message) => console.log(message));
 
 // STEP 4: Create a file using fs module and async/await.
 
-const fs = require("fs").promises;
-
 const writeFileAsAsync = async () => {
   try {
     await fs.writeFile(
@@ -30,3 +29,5 @@ const writeFileAsAsync = async () => {
 };
 
 writeFileAsAsync();
+
+// STEP 6: Push thse 2 commits to a remote repository..
